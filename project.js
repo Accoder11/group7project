efd3357737093762f31c25ba16ba70f6fb125a06
Alma Cleto <emscleto11@gmail.com>
2023-03-07 21:27:47 +0800

    updated

--- project.js
+++ project.js
@@ -2,30 +2,13 @@ var nav = document.querySelector('nav');
 
 window.addEventListener('scroll', function() {
     if(this.window.pageYOffset > 100) {
-        nav.classList.add('bg-black', 'shadow');
+        nav.classList.add('bg-warning', 'shadow');
     } else {
-        nav.classList.remove('bg-black', 'shadow');
+        nav.classList.remove('bg-warning', 'shadow');
     }
   });
 
 
-// sample rating form
-
-// const btn = document.querySelector("button");
-// const post = document.querySelector(".post");
-// const widget = document.querySelector(".star-widget");
-// const editBtn = document.querySelector(".edit");
-// btn.onclick = ()=>{
-//   widget.style.display = "none";
-//   post.style.display = "block";
-//   editBtn.onclick = ()=>{
-//     widget.style.display = "block";
-//     post.style.display = "none";
-//   }
-//   return false;
-// }
-
-// 2nd recipe rating
 
 const btn1 = document.querySelector("btns1");
 const post1 = document.querySelector(".post1");
